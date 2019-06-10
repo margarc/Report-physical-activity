3d4b9e1b32c750d446c6528abf18da8e37b40ea8
Margarita <margarita.cariolou@gmail.com>
2019-06-10 15:02:49 +0100

    Add files via upload

---
+++ forest_draft1.R
@@ -0,0 +1,112 @@
+getwd()
+setwd("\\\\icnas2.cc.ic.ac.uk/mc6515/Desktop/survivors/pa_report")
+
+# Load packages 
+library(meta)
+library(rmeta)
+library(gdata)
+library(metafor)
+require(gdata)
+library(ggplot2)
+library(readxl)
+library(pwr)
+library(xlsx)
+library(ggplot2)
+library(writexl)
+library(stringr)
+library(tidyverse)
+library(dosresmeta)
+?`dosresmeta-package`
+dir.create("plots")
+
+test <- read_excel("physactiv_forestpost.xlsx",sheet="modvig_bremortal")
+dim(test) # 35 , 110 
+
+
+results <- subset(test, highvslow=="yes")
+dim(results) # 6, 110
+View(results)
+
+View(results)
+
+resultsnew <- results %>%
+ select(Author, Year, StudyDescription, MenoStatus, SizeStudy, Number_Women, LengthFollowUp,
+        LengthFollowUnits, LengthFollowType, ResultsNumber, 
+        RR, CILow, CIUpper, Reference, maxFromReference, analysis_type)
+View(resultsnew)
+
+dim(resultsnew) # 6 , 16 cols
+
+# add new row with the results of Maliniak that was caclulated with the Fixed effects model 
+newRow <- data.frame(Author="Maliniak", Year="2018", StudyDescription="Cancer Prevention Study II Nutrition Cohort", 
+                     MenoStatus="Post-menopausal", SizeStudy=5254, Number_Women=5254, 
+                     LengthFollowUp=13.3, LengthFollowUnits="years", LengthFollowType="median", 
+                     ResultsNumber="7124&7128", RR=0.823, CILow=0.582, CIUpper=1.163, 
+                     Reference=0, maxFromReference=1, analysis_type="moderate_and_vig_bremortal")
+
+
+resultsnew <- rbind(resultsnew,newRow)
+View(resultsnew)
+
+# Replace study names 
+resultsnew$`Study Description` <- ifelse(resultsnew$`Study Description`=="Cancer Prevention Study II Nutrition Cohort", "CPS-II Nutrition Cohort",
+                                         ifelse(resultsnew$`Study Description`=="Women's Heath Initiave", "WHI",
+                                                ifelse(resultsnew$`Study Description`=="Colloborative Women's Longevity Study", "CWLS",
+                                                       ifelse(resultsnew$`Study Description`=="Women's Healthy Eating and Living Study", "WHEL",
+                                                              ifelse(resultsnew$`Study Description`=="LACE", "LACE", NA)))))
+
+
+#######################
+
+
+
+
+vigmodtotmort <- metagen(TE = vigmod_totmort$logRR, seTE = vigmod_totmort$sei,  data=vigmod_totmort, 
+                         studlab = vigmod_totmort$Author, sm ="RR", prediction = F,
+                         byvar = analysis_type,
+                         comb.fixed = F, method.tau="DL")
+
+vigmodtotmort
+
+print.meta(vigmodtotmort, cilayout(bracket = "(", separator = ","))                                               
+
+pdf(file = "plots/forest_modvig_totmort.pdf", width = 9, height = 7)
+
+forest.meta(vigmodtotmort, studlab = TRUE, 
+            comb.random = vigmodtotmort$comb.random,
+            lyt.random = TRUE,
+            type.study="square",
+            squaresize=0.5,
+            type.random="diamond",
+            subgroup = TRUE, # we need to see the pooled results of subgroups
+            overall = FALSE, # but not an overall pooling as studies are repeated 
+            print.subgroup.labels = TRUE,
+            bylab = "",
+            text.random = "", # this line to delete text for Random effects model
+            col.study="black", 
+            col.square="black", 
+            col.inside="white", 
+            col.diamond="transparent", 
+            col.diamond.lines="black",
+            col.label.right="black",
+            col.label.left="black",
+            col.by="black", # specify the color of the results for subgroups
+            smlab="", # this to delete the risk ratio at the top of the plot
+            leftcols=c("studlab", "Year"),# to remove TE and seTE from plot
+            rightcols=c("effect", "ci", "w.random","Study Description", "contrast"),
+            rightlabs=c("RR","95% CI", "% Weight", "Study Description", "  contrast (high vs low)"),
+            hetlab = "Overall: ", 
+            lwd=0.9,
+            print.I2 = vigmodtotmort$comb.random,
+            plotwidth="5cm",
+            colgap.forest.left="0.5cm",
+            colgap.forest.right="0.000005cm",
+            colgap.right="0.5cm",
+            print.I2.ci = FALSE, 
+            print.tau2 = FALSE, 
+            print.Q = FALSE,
+            digits = 2, # specifies the number of digits on the plot after comma
+            fontsize = 7)
+
+
+
