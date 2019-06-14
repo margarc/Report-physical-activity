359f5edf8cce2fdc04d42a43eca9cf12a4ac4236
Margarita <margarita.cariolou@gmail.com>
2019-06-14 16:08:23 +0100

    Update doseres.R

--- doseres.R
+++ doseres.R
@@ -16,3 +16,15 @@ exp(-0.0314)^10 # 0.730519
 
 # get the upper confidence interval per 10 g/day
 exp(0.0043)^10  #  1.043938
+
+
+# For fibre and physical activity open ended categories, if the lower limit of the first one is missing then put the lower bound to be ZERO. 
+
+# To calculate the upper bound of a category/quantile add the width of the previous category/quantile i.e. [the upper-lower level/number]
+# Then estimate the midpoint by adding the newly calculated upper bound by the lower bound 
+
+# Alternatively to calculate the midpoint directly: 
+# lower bound of the top open ended cat/quant+[(upper-lower bound of the previous category)/2]
+
+
+
